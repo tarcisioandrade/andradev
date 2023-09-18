c2a83834974eb0b6c14ecd8036f10759c3bbb7b6
Tarcisio <tarcisioandrade016@gmail.com>
2023-09-17 23:53:32 -0300

    feat: footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-gradient-to-b from-sky-950 to-sky-950/50">
+      <footer className="h-52 max-w-[1152px] mx-auto px-4">Footer</footer>
+    </div>
+  );
+};
+
+export default Footer;
