e50db552d85d8b6b381f3b32e50c9c1b9fe129f6
Tarcisio <tarcisioandrade016@gmail.com>
2023-09-27 15:33:59 -0300

    image metaData

--- src/app/(post-page)/[slug]/page.tsx
+++ src/app/(post-page)/[slug]/page.tsx
@@ -1,15 +1,15 @@
-import { format, parseISO } from "date-fns";
-import { ptBR } from "date-fns/locale";
-import { allPosts } from "contentlayer/generated";
-import { notFound } from "next/navigation";
+import ButtonScrollTop from "@/components/button-scroll-top";
 import MDXRender from "@/components/mdx-render";
 import MenuToc from "@/components/menu-toc";
-import { Metadata } from "next";
-import { Timer, Tags } from "lucide-react";
 import { Post } from "@/types/post";
-import Link from "next/link";
 import { slugger } from "@/utils/slugger";
-import ButtonScrollTop from "@/components/button-scroll-top";
+import { allPosts } from "contentlayer/generated";
+import { format, parseISO } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { Tags, Timer } from "lucide-react";
+import { Metadata } from "next";
+import Link from "next/link";
+import { notFound } from "next/navigation";
 
 export const generateStaticParams = async () =>
   allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
@@ -20,66 +20,73 @@ export const generateMetadata = ({
   params: { slug: string };
 }): Metadata => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
-  if (!post) notFound();
+  if (!post) {
+    notFound();
+  }
+
+  const opImage = post.image.filePath.replaceAll("../public", "");
 
   return {
-    title: post.title,
     description: post.description,
-    twitter: { title: post.title, description: post.description },
     openGraph: {
-      title: post.title,
       description: post.description,
-      type: "article",
+      images: opImage,
       publishedTime: post.publishedAt,
+      title: post.title,
+      type: "article",
     },
+    title: post.title,
+    twitter: { description: post.description, title: post.title },
   };
 };
 
 const PostLayout = ({ params }: { params: { slug: string } }) => {
   const post = allPosts.find(
-    (post) => post._raw.flattenedPath === params.slug
+    (post) => post._raw.flattenedPath === params.slug,
   ) as Post;
 
-  if (!post) notFound();
+  if (!post) {
+    notFound();
+  }
 
   const readTimeTranslate = post.readingTime.text
     .split(" ")[0]
     .concat(" minutos de leitura");
 
   return (
-    <main className="mx-auto main-container px-4 py-24 xl:py-36 xl:grid xl:grid-cols-[minmax(400px,_680px)_1fr] xl:gap-20">
+    <main className="main-container mx-auto px-4 py-24 xl:grid xl:grid-cols-[minmax(400px,_680px)_1fr] xl:gap-20 xl:py-36">
       <div>
         <div className="mb-8">
           <time
+            className="mb-4 block text-base text-gray-500"
             dateTime={post.publishedAt}
-            className="text-base text-gray-500 mb-4 block"
           >
             {format(
               parseISO(post.publishedAt),
               "EEEE, dd 'de' LLLL 'de' yyyy",
               {
                 locale: ptBR,
-              }
+              },
             )}
           </time>
           <h1
+            className="scroll-mt-32 text-4xl font-bold leading-tight lg:text-5xl"
             id="introduction"
-            className="text-4xl lg:text-5xl font-bold scroll-mt-32 leading-tight"
           >
             {post.title}
           </h1>
-          <ul className="flex flex-col md:flex-row gap-4 xl:gap-8 mt-4 text-sm text-gray-500 dark:text-gray-400 w-fit rounded">
+          <ul className="mt-4 flex w-fit flex-col gap-4 rounded text-sm text-gray-500 dark:text-gray-400 md:flex-row xl:gap-8">
             <li className="flex gap-1">
-              <Timer size={20} className="text-blue-500 dark:text-amber-500" />
+              <Timer className="text-blue-500 dark:text-amber-500" size={20} />
               <time>{readTimeTranslate}</time>
             </li>
             <li className="flex gap-1">
-              <Tags size={20} className="text-blue-500 dark:text-amber-500" />
+              <Tags className="text-blue-500 dark:text-amber-500" size={20} />
               <div>
                 {post.categories.map((categ, i) => (
                   <span key={`${categ}-${i}`}>
                     <Link
-                      className="capitalize text-blue-500 hover:underline underline-offset-4"
+                      className="capitalize text-blue-500 underline-offset-4 hover:underline"
                       href={`/category/${slugger(categ)}`}
                     >
                       {categ}
