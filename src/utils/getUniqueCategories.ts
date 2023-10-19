78a0c74f329c5a5f8f8fd9cd63e27c9eed33aca8
Tarcisio <tarcisioandrade016@gmail.com>
2023-10-19 15:32:33 -0300

    fix: unpublished post categories are no longer shown

--- src/utils/getUniqueCategories.ts
+++ src/utils/getUniqueCategories.ts
@@ -1,12 +1,15 @@
 import { allPosts } from "contentlayer/generated";
 
 export const getUniqueCategories = () => {
-  const allCategoriesPosts = allPosts.map(({ categories }) => categories);
+  const allCategoriesPosts = allPosts
+    .filter(({ isPublished }) => isPublished)
+    .map(({ categories }) => categories);
+
   const uniqueCategories = new Set<string>();
 
   allCategoriesPosts.flat().map((cat) => uniqueCategories.add(cat));
 
   const categories = Array.from(uniqueCategories);
 
-  return categories;
+  return categories.sort();
 };
